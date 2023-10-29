81fc365840eb9010f0f033adde5da21336f92215
Gaël Hébert-Furoy <118003096+Vaxs02@users.noreply.github.com>
2023-10-29 11:28:29 -0400

    Update distromanager.js

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -2,7 +2,7 @@ const { DistributionAPI } = require('helios-core/common')
 
 const ConfigManager = require('./configmanager')
 
-exports.REMOTE_DISTRO_URL = 'https://dexagroup.wstr.fr/nebula2/distribution.json'
+exports.REMOTE_DISTRO_URL = 'VOTRE LIEN VERS VOTRE DISTRIBUTION'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
